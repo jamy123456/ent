196c4cee2d0584f67be51875a877a6f5fdd0829a
jamy123456 <990134943@qq.com>
2019-05-11 09:27:38 +0800

    Initial commit

---
+++ src/main/java/ent/HelloApplication.java
@@ -0,0 +1,18 @@
+package ent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * 作者:jamy
+ * 时间:2019/5/10 20:51
+ * 描述:
+ */
+
+@SpringBootApplication
+public class HelloApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class, args);
+    }
+}
---
+++ src/main/java/ent/controller/HelloWorldContoller.java
@@ -0,0 +1,21 @@
+package ent.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * 作者:jamy
+ * 时间:2019/5/10 20:48
+ * 描述:
+ */
+
+@RestController
+public class HelloWorldContoller {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "helloworld";
+    }
+}
